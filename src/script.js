e2366cf20d49041def54f87d2e61bab9dd9000b2
jcas96 <joelcastro.jsc@gmail.com>
2024-10-01 14:43:58 -0700

    New files

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const clientId = "Spotify Client ID";
+const clientId = "5f56bd6a2551494ca8579167c145bedf";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
@@ -32,7 +32,7 @@ export async function redirectToAuthCodeFlow(clientId){
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri","https://jcas96.github.io/Top-Artists/callback");
+    params.append("redirect_uri","http://localhost:5173/callback");
     params.append("scope","user-read-private user-read-email user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -66,7 +66,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "https://jcas96.github.io/Top-Artists/callback");
+    params.append("redirect_uri", "http://localhost:5173/callback");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
